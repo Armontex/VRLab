9c756ca2fb72a8d7841989b2f843d92d209bb848
Махонько Максим Андреевич <windle1337@gmail.com>
2025-11-30 22:35:25 +0500

    Добавил частично префабы

---
+++ Assets/Scripts/Custom/CylinderMaterialType.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace UnityEngine.Custom
+{
+    public class CylinderMaterialType
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/RandomCylinder.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class RandomCylinder : MonoBehaviour
+{
+
+    public enum MaterialType
+    {
+        Wood,
+        Steel,
+        Plastic
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
